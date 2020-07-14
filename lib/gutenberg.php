090423f86632058aa13c73fe8194ebb370759652
Chris Baldelomar <chris@webplant.media>
2020-07-14 00:38:56 -0500

    update

--- lib/gutenberg.php
+++ lib/gutenberg.php
@@ -2,19 +2,19 @@
 /**
  * Gutenberg theme support.
  *
- * @package Leadership Pro
+ * @package Empower Pro Blocks
  * @author  Web Plant Media
  * @license GPL-2.0-or-later
  * @link    https://webplantmedia.com/
  */
 
-add_action( 'wp_enqueue_scripts', 'leadership_pro_enqueue_gutenberg_frontend_styles' );
+add_action( 'wp_enqueue_scripts', 'empower_pro_blocks_enqueue_gutenberg_frontend_styles' );
 /**
  * Enqueues Gutenberg front-end styles.
  *
  * @since 1.1.0
  */
-function leadership_pro_enqueue_gutenberg_frontend_styles() {
+function empower_pro_blocks_enqueue_gutenberg_frontend_styles() {
 
 
 }
@@ -35,31 +35,31 @@ add_theme_support( 'responsive-embeds' );
 /* add_theme_support('disable-custom-font-sizes'); */
 
 // Get appearance config array.
-global $leadership_pro_appearance;
-$leadership_pro_appearance = genesis_get_config( 'appearance' );
+global $empower_pro_blocks_appearance;
+$empower_pro_blocks_appearance = genesis_get_config( 'appearance' );
 
 // Adds support for editor font sizes.
-add_theme_support( 'editor-font-sizes', $leadership_pro_appearance['editor-font-sizes'] );
+add_theme_support( 'editor-font-sizes', $empower_pro_blocks_appearance['editor-font-sizes'] );
 
 // Adds support for editor color palette.
-add_theme_support( 'editor-color-palette', $leadership_pro_appearance['editor-color-palette'] );
+add_theme_support( 'editor-color-palette', $empower_pro_blocks_appearance['editor-color-palette'] );
 
 // Add inline style for gutenberg blocks.
 require_once get_stylesheet_directory() . '/css/gutenberg-inline-styles.php';
 require_once get_stylesheet_directory() . '/css/gutenberg-inline-button-styles.php';
 
-add_action( 'after_setup_theme', 'leadership_pro_content_width', 0 );
+add_action( 'after_setup_theme', 'empower_pro_blocks_content_width', 0 );
 /**
  * Set content width to match the “wide” Gutenberg block width.
  */
-function leadership_pro_content_width() {
-	global $leadership_pro_appearance;
+function empower_pro_blocks_content_width() {
+	global $empower_pro_blocks_appearance;
 
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound -- See https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/924
-	$GLOBALS['content_width'] = apply_filters( 'leadership_pro_content_width', $leadership_pro_appearance['content-width'] );
+	$GLOBALS['content_width'] = apply_filters( 'empower_pro_blocks_content_width', $empower_pro_blocks_appearance['content-width'] );
 }
 
-add_action( 'enqueue_block_editor_assets', 'leadership_pro_custom_gutenberg_admin_css' );
+add_action( 'enqueue_block_editor_assets', 'empower_pro_blocks_custom_gutenberg_admin_css' );
 /**
  * Output back-end inline styles for link state.
  *
@@ -68,26 +68,26 @@ add_action( 'enqueue_block_editor_assets', 'leadership_pro_custom_gutenberg_admi
  *
  * @since 1.1.0
  */
-function leadership_pro_custom_gutenberg_admin_css() {
-	global $leadership_pro_appearance;
+function empower_pro_blocks_custom_gutenberg_admin_css() {
+	global $empower_pro_blocks_appearance;
 
 	wp_enqueue_style(
-		'leadership-pro-gutenberg-fonts',
-		$leadership_pro_appearance['fonts-url'],
+		'empower-pro-blocks-gutenberg-fonts',
+		$empower_pro_blocks_appearance['fonts-url'],
 		array(),
 		CHILD_THEME_VERSION
 	);
 
-	wp_enqueue_style( 'ionicons', $leadership_pro_appearance['icons-url'], array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'ionicons', $empower_pro_blocks_appearance['icons-url'], array(), CHILD_THEME_VERSION );
 
-	$css = leadership_pro_get_gutenberg_css( $leadership_pro_appearance );
-	$css .= leadership_pro_get_gutenberg_button_css( $leadership_pro_appearance );
+	$css = empower_pro_blocks_get_gutenberg_css( $empower_pro_blocks_appearance );
+	$css .= empower_pro_blocks_get_gutenberg_button_css( $empower_pro_blocks_appearance );
 
-	wp_add_inline_style( 'leadership-pro-gutenberg-fonts', $css );
+	wp_add_inline_style( 'empower-pro-blocks-gutenberg-fonts', $css );
 
 }
 
-add_action( 'wp_enqueue_scripts', 'leadership_pro_custom_gutenberg_css' );
+add_action( 'wp_enqueue_scripts', 'empower_pro_blocks_custom_gutenberg_css' );
 /**
  * Output front-end inline styles for `editor-color-palette` colors.
  *
@@ -95,18 +95,18 @@ add_action( 'wp_enqueue_scripts', 'leadership_pro_custom_gutenberg_css' );
  *
  * @since 1.1.0
  */
-function leadership_pro_custom_gutenberg_css() {
-	global $leadership_pro_appearance;
+function empower_pro_blocks_custom_gutenberg_css() {
+	global $empower_pro_blocks_appearance;
 
 	wp_enqueue_style(
-		'leadership-pro-gutenberg',
+		'empower-pro-blocks-gutenberg',
 		get_stylesheet_directory_uri() . '/css/style-blocks.css',
-		array( 'leadership-pro-main' ),
+		array( 'empower-pro-blocks-main' ),
 		CHILD_THEME_VERSION
 	);
 
-	$css = leadership_pro_get_gutenberg_css( $leadership_pro_appearance );
-	$css .= leadership_pro_get_gutenberg_button_css( $leadership_pro_appearance );
+	$css = empower_pro_blocks_get_gutenberg_css( $empower_pro_blocks_appearance );
+	$css .= empower_pro_blocks_get_gutenberg_button_css( $empower_pro_blocks_appearance );
 
-	wp_add_inline_style( 'leadership-pro-main', $css );
+	wp_add_inline_style( 'empower-pro-blocks-main', $css );
 }
