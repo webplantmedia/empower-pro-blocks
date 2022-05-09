c1b33c85f906f6043c362c4ae91c8dd32ba186d3
Chris Baldelomar <chris@webplant.media>
2022-05-09 07:09:49 -0500

    update

--- empower-pro-blocks.php
+++ empower-pro-blocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     Empower Pro Blocks
  * Description:     Custom blocks for Empower Pro Blocks theme.
- * Version:         1.1.90
+ * Version:         1.1.91
  * Author:          Web Plant Media
  * License:         GPL-2.0-or-later
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
