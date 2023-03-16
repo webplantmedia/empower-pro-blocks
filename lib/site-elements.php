0e7df5bc4548856662c680f161d4af1a6a7854ec
Chris Baldelomar <chris@webplant.media>
2023-03-16 06:50:16 -0500

    update

--- lib/site-elements.php
+++ lib/site-elements.php
@@ -12,3 +12,42 @@ function empower_pro_blocks_fetch_all_site_elements()
 
 	return array("" => "None") + $list;
 }
+
+function gallop_create_post_type()
+{
+
+	$args = apply_filters(
+		'gallop_post_type_args',
+		array(
+			'labels'        => array(
+				'name'               => __('Site Elements', 'empress-pro-blocks'),
+				'singular_name'      => __('Site Element', 'empress-pro-blocks'),
+				'add_new'            => __('Add New', 'empress-pro-blocks'),
+				'add_new_item'       => __('Add New Site Element', 'empress-pro-blocks'),
+				'edit'               => __('Edit', 'empress-pro-blocks'),
+				'edit_item'          => __('Edit Site Element', 'empress-pro-blocks'),
+				'new_item'           => __('New Site Element', 'empress-pro-blocks'),
+				'view'               => __('View Site Element', 'empress-pro-blocks'),
+				'view_item'          => __('View Site Element', 'empress-pro-blocks'),
+				'search_items'       => __('Search Site Elements', 'empress-pro-blocks'),
+				'not_found'          => __('No site elements found', 'empress-pro-blocks'),
+				'not_found_in_trash' => __('No site elements found in Trash', 'empress-pro-blocks'),
+			),
+			'public'        => true,
+			'exclude_from_search' => true,
+			'publicly_queryable' => false,
+			'show_ui' => true,
+			'show_in_menu' => true,
+			'query_var'     => true,
+			'menu_position' => 6,
+			'menu_icon'     => 'dashicons-welcome-widgets-menus',
+			'has_archive'   => false,
+			'show_in_rest'  => true,
+			'supports'      => array('title', 'page-attributes', 'author', 'editor', 'excerpt', 'revisions', 'thumbnail'),
+		)
+	);
+
+	register_post_type('site_element', $args);
+}
+
+add_action('init', 'gallop_create_post_type');
