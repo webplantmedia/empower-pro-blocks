0a4f50d218e21b92a883c6971f4cc4ad88b1ffbe
Chris Baldelomar <chris@webplant.media>
2023-06-15 18:48:47 -0500

    update

--- lib/headings.php
+++ lib/headings.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Empower Pro Blocks.
  *
@@ -15,7 +16,8 @@
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_header_title_wrap() {
+function empower_pro_blocks_header_title_wrap()
+{
 	genesis_markup(
 		array(
 			'open'    => '<div %s><div class="wrap"><div class="inner-container">',
@@ -29,7 +31,8 @@ function empower_pro_blocks_header_title_wrap() {
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_header_title_end_wrap() {
+function empower_pro_blocks_header_title_end_wrap()
+{
 	genesis_markup(
 		array(
 			'close'   => '</div></div></div>',
@@ -38,7 +41,7 @@ function empower_pro_blocks_header_title_end_wrap() {
 	);
 }
 
-add_filter( 'genesis_attr_empower-pro-blocks-page-title', 'empower_pro_blocks_add_header_attributes' );
+add_filter('genesis_attr_empower-pro-blocks-page-title', 'empower_pro_blocks_add_header_attributes');
 /**
  * Adds custom attributes for the empower-pro-blocks header wrap.
  *
@@ -47,14 +50,14 @@ add_filter( 'genesis_attr_empower-pro-blocks-page-title', 'empower_pro_blocks_ad
  * @param array $attributes The element attributes.
  * @return array $attributes The element attributes.
  */
-function empower_pro_blocks_add_header_attributes( $attributes ) {
+function empower_pro_blocks_add_header_attributes($attributes)
+{
 
 	$attributes['id'] = 'empower-pro-blocks-page-title';
 	return $attributes;
-
 }
 
-add_filter( 'genesis_attr_entry', 'empower_pro_blocks_entry_attributes', 10, 3 );
+add_filter('genesis_attr_entry', 'empower_pro_blocks_entry_attributes', 10, 3);
 /**
  * Add itemref attribute to link entry-title.
  *
@@ -65,13 +68,13 @@ add_filter( 'genesis_attr_entry', 'empower_pro_blocks_entry_attributes', 10, 3 )
  * @param array $args The attribute arguments.
  * @return array Amended attributes for entry element.
  */
-function empower_pro_blocks_entry_attributes( $attributes, $context, $args ) {
+function empower_pro_blocks_entry_attributes($attributes, $context, $args)
+{
 
-	if ( is_singular() && ! isset( $args['params']['is_widget'] ) ) {
+	if (is_singular() && !isset($args['params']['is_widget'])) {
 		$attributes['itemref'] = 'empower-pro-blocks-page-title';
 	}
 	return $attributes;
-
 }
 
 /**
@@ -82,103 +85,102 @@ function empower_pro_blocks_entry_attributes( $attributes, $context, $args ) {
  * @param array $classes Original body classes.
  * @return array Modified body classes.
  */
-function empower_pro_blocks_has_featured_image( $classes ) {
+function empower_pro_blocks_has_featured_image($classes)
+{
 
 	global $empower_pro_blocks_appearance;
 
-	if ( ! empower_pro_blocks_has_post_thumbnail() ) {
+	if (!empower_pro_blocks_has_post_thumbnail()) {
 		return $classes;
 	}
 
 	$page_template = get_page_template();
 
-	if ( is_singular( 'post' ) && $empower_pro_blocks_appearance['post-image'] ) {
+	if (is_singular('post') && $empower_pro_blocks_appearance['post-image']) {
 		$classes[] = 'has-featured-image';
 	}
 
-	if ( ( is_singular( 'page' ) || is_404() ) && $empower_pro_blocks_appearance['page-image'] ) {
+	if ((is_singular('page') || is_404()) && $empower_pro_blocks_appearance['page-image']) {
 		$classes[] = 'has-featured-image';
 	}
 
-	if ( is_singular( 'portfolio' ) && $empower_pro_blocks_appearance['portfolio-image'] ) {
+	if (is_singular('portfolio') && $empower_pro_blocks_appearance['portfolio-image']) {
 		$classes[] = 'has-featured-image';
 	}
 
-	if ( is_singular( 'event' ) && $empower_pro_blocks_appearance['event-image'] ) {
+	if (is_singular('event') && $empower_pro_blocks_appearance['event-image']) {
 		$classes[] = 'has-featured-image';
 	}
 
-	if ( is_page_template( 'page_blog.php' ) || is_archive() || is_home() ) {
+	if (is_page_template('page_blog.php') || is_archive() || is_home()) {
 		$classes[] = 'has-featured-images'; // Note plural for archives.
 	}
 
 	return $classes;
-
 }
 
 
-add_action( 'genesis_meta', 'empower_pro_blocks_move_page_headers' );
+add_action('genesis_meta', 'empower_pro_blocks_move_page_headers');
 /**
  * Moves page title above content wrap.
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_move_page_headers() {
+function empower_pro_blocks_move_page_headers()
+{
 
-	add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_wrap', 1 );
-	add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_end_wrap', 18 );
+	add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_wrap', 1);
+	add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_end_wrap', 18);
 
-	if ( ( is_page() || is_single() ) && ! is_page_template( 'page_blog.php' ) ) {
-		remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-		remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-		remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
-		remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+	if ((is_page() || is_single()) && !is_page_template('page_blog.php')) {
+		remove_action('genesis_entry_header', 'genesis_entry_header_markup_open', 5);
+		remove_action('genesis_entry_header', 'genesis_entry_header_markup_close', 15);
+		remove_action('genesis_entry_header', 'genesis_do_post_title');
+		remove_action('genesis_entry_header', 'genesis_post_info', 12);
 
-		add_action( 'genesis_before_content_sidebar_wrap', 'genesis_post_info', 4 );
-		add_action( 'genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_open', 5 );
-		add_action( 'genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_close', 15 );
-		add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_post_title' );
-		add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_featured_image', 16 );
+		add_action('genesis_before_content_sidebar_wrap', 'genesis_post_info', 4);
+		add_action('genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_open', 5);
+		add_action('genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_close', 15);
+		add_action('genesis_before_content_sidebar_wrap', 'genesis_do_post_title');
+		add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_featured_image', 16);
 
 		// Adds has-featured-image body class.
-		add_filter( 'body_class', 'empower_pro_blocks_has_featured_image', 10, 1 );
-
+		add_filter('body_class', 'empower_pro_blocks_has_featured_image', 10, 1);
 	} else {
-		remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-		add_action( 'genesis_entry_header', 'genesis_post_info', 8 );
+		remove_action('genesis_entry_header', 'genesis_post_info', 12);
+		add_action('genesis_entry_header', 'genesis_post_info', 8);
 	}
 
-	if ( is_singular( 'product' ) || is_singular( 'books' ) ) {
-		remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_post_info', 4 );
+	if (is_singular('product') || is_singular('books')) {
+		remove_action('genesis_before_content_sidebar_wrap', 'genesis_post_info', 4);
 	}
 
-	if ( is_singular( 'books' ) ) {
-		add_action( 'genesis_before_content_sidebar_wrap', 'genesis_author_pro_do_by_line' );
+	if (is_singular('books')) {
+		add_action('genesis_before_content_sidebar_wrap', 'genesis_author_pro_do_by_line');
 	}
 
-	if ( is_404() ) {
-		add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_featured_image', 16 );
-		add_filter( 'body_class', 'empower_pro_blocks_has_featured_image', 10, 1 );
+	if (is_404()) {
+		add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_featured_image', 16);
+		add_filter('body_class', 'empower_pro_blocks_has_featured_image', 10, 1);
 	}
 
-	$is_woocommerce_shop = class_exists( 'WooCommerce' ) && is_post_type_archive( 'product' );
+	$is_woocommerce_shop = class_exists('WooCommerce') && is_post_type_archive('product');
 
-	if ( is_search() ) {
-		remove_action( 'genesis_before_loop', 'genesis_do_search_title' );
+	if (is_search()) {
+		remove_action('genesis_before_loop', 'genesis_do_search_title');
 
-		if ( ! $is_woocommerce_shop ) {
-			add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_search_title' );
+		if (!$is_woocommerce_shop) {
+			add_action('genesis_before_content_sidebar_wrap', 'genesis_do_search_title');
 		}
 	}
 
-	if ( $is_woocommerce_shop ) {
-		add_filter( 'woocommerce_show_page_title', '__return_false' );
-		add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_woocommerce_title' );
+	if ($is_woocommerce_shop) {
+		add_filter('woocommerce_show_page_title', '__return_false');
+		add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_woocommerce_title');
 	}
-
 }
 
-add_action( 'genesis_before_while', 'empower_pro_blocks_remove_author_pro_header' );
+add_action('genesis_before_while', 'empower_pro_blocks_remove_author_pro_header');
 /**
  * Remove duplicate title markup on book singles.
  *
@@ -190,17 +192,17 @@ add_action( 'genesis_before_while', 'empower_pro_blocks_remove_author_pro_header
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_remove_author_pro_header() {
+function empower_pro_blocks_remove_author_pro_header()
+{
 
-	if ( ! is_singular( 'books' ) ) {
+	if (!is_singular('books')) {
 		return;
 	}
 
-	remove_action( 'genesis_before_entry_content', 'genesis_entry_header_markup_open', 5 );
-	remove_action( 'genesis_before_entry_content', 'genesis_do_post_title' );
-	remove_action( 'genesis_before_entry_content', 'genesis_author_pro_do_by_line', 12 );
-	remove_action( 'genesis_before_entry_content', 'genesis_entry_header_markup_close', 15 );
-
+	remove_action('genesis_before_entry_content', 'genesis_entry_header_markup_open', 5);
+	remove_action('genesis_before_entry_content', 'genesis_do_post_title');
+	remove_action('genesis_before_entry_content', 'genesis_author_pro_do_by_line', 12);
+	remove_action('genesis_before_entry_content', 'genesis_entry_header_markup_close', 15);
 }
 
 /**
@@ -210,54 +212,54 @@ function empower_pro_blocks_remove_author_pro_header() {
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_woocommerce_title() {
+function empower_pro_blocks_woocommerce_title()
+{
 
 	echo '<h1 class="woocommerce-products-header__title page-title">';
 	woocommerce_page_title();
 	echo '</h1>';
-
 }
 
-add_action( 'genesis_loop', 'empower_pro_blocks_remove_404_title' );
+add_action('genesis_loop', 'empower_pro_blocks_remove_404_title');
 /**
  * Removes default 404 entry title.
  *
  * @since  1.0.0
  */
-function empower_pro_blocks_remove_404_title() {
+function empower_pro_blocks_remove_404_title()
+{
 
-	if ( ! is_404() ) {
+	if (!is_404()) {
 		return;
 	}
-	add_filter( 'genesis_markup_entry-title_open', '__return_false' );
-	add_filter( 'genesis_markup_entry-title_close', '__return_false' );
-	add_filter( 'genesis_404_entry_title', '__return_false' );
-
+	add_filter('genesis_markup_entry-title_open', '__return_false');
+	add_filter('genesis_markup_entry-title_close', '__return_false');
+	add_filter('genesis_404_entry_title', '__return_false');
 }
 
-add_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_404_title' );
+add_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_404_title');
 /**
  * Outputs 404 entry title in new position.
  *
  * @since  1.0.0
  */
-function empower_pro_blocks_404_title() {
+function empower_pro_blocks_404_title()
+{
 
-	if ( ! is_404() ) {
+	if (!is_404()) {
 		return;
 	}
 	genesis_markup(
 		array(
 			'open'    => '<h1 %s>',
 			'close'   => '</h1>',
-			'content' => apply_filters( 'empower_pro_blocks_404_entry_title', __( '404 Page Not Found', 'empower-pro-blocks' ) ),
+			'content' => apply_filters('empower_pro_blocks_404_entry_title', __('404 Page Not Found', 'empower-pro-blocks')),
 			'context' => 'entry-title',
 		)
 	);
-
 }
 
-add_action( 'genesis_after_loop', 'empower_pro_blocks_remove_404_title_filters' );
+add_action('genesis_after_loop', 'empower_pro_blocks_remove_404_title_filters');
 /**
  * Removes 404 entry-title filters.
  *
@@ -266,14 +268,14 @@ add_action( 'genesis_after_loop', 'empower_pro_blocks_remove_404_title_filters' 
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_remove_404_title_filters() {
+function empower_pro_blocks_remove_404_title_filters()
+{
 
-	remove_filter( 'genesis_markup_entry-title_open', '__return_false' );
-	remove_filter( 'genesis_markup_entry-title_close', '__return_false' );
-
+	remove_filter('genesis_markup_entry-title_open', '__return_false');
+	remove_filter('genesis_markup_entry-title_close', '__return_false');
 }
 
-add_action( 'be_title_toggle_remove', 'empower_pro_blocks_integrate_genesis_title_toggle' );
+add_action('be_title_toggle_remove', 'empower_pro_blocks_integrate_genesis_title_toggle');
 /**
  * Integrates with Genesis Title Toggle.
  *
@@ -284,16 +286,16 @@ add_action( 'be_title_toggle_remove', 'empower_pro_blocks_integrate_genesis_titl
  * @see https://wordpress.org/plugins/genesis-title-toggle/
  * @see https://www.billerickson.net/code/genesis-title-toggle-theme-integration
  */
-function empower_pro_blocks_integrate_genesis_title_toggle() {
+function empower_pro_blocks_integrate_genesis_title_toggle()
+{
 
-	remove_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_wrap', 1 );
-	remove_action( 'genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_end_wrap', 18 );
-	remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_open', 5 );
-	remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_post_title' );
-	remove_action( 'genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_close', 15 );
-	add_filter( 'genesis_skip_links_output', 'empower_pro_blocks_adjust_skip_links' );
-	remove_filter( 'genesis_attr_entry', 'empower_pro_blocks_entry_attributes' );
-
+	remove_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_wrap', 1);
+	remove_action('genesis_before_content_sidebar_wrap', 'empower_pro_blocks_header_title_end_wrap', 18);
+	remove_action('genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_open', 5);
+	remove_action('genesis_before_content_sidebar_wrap', 'genesis_do_post_title');
+	remove_action('genesis_before_content_sidebar_wrap', 'genesis_entry_header_markup_close', 15);
+	add_filter('genesis_skip_links_output', 'empower_pro_blocks_adjust_skip_links');
+	remove_filter('genesis_attr_entry', 'empower_pro_blocks_entry_attributes');
 }
 
 /**
@@ -304,32 +306,32 @@ function empower_pro_blocks_integrate_genesis_title_toggle() {
  * @param array $links The list of skip links.
  * @return array $links The modified list of skip links.
  */
-function empower_pro_blocks_adjust_skip_links( $links ) {
+function empower_pro_blocks_adjust_skip_links($links)
+{
 
-	unset( $links['empower-pro-blocks-page-title'] );
-	array_unshift( $links, array( 'genesis-content' => __( 'Skip to content', 'empower-pro-blocks' ) ) );
+	unset($links['empower-pro-blocks-page-title']);
+	array_unshift($links, array('genesis-content' => __('Skip to content', 'empower-pro-blocks')));
 
 	return $links;
-
 }
 
 // Moves titles outside content-sidebar-wrap.
-remove_action( 'genesis_before_loop', 'genesis_do_cpt_archive_title_description' );
-remove_action( 'genesis_before_loop', 'genesis_do_posts_page_heading' );
-remove_action( 'genesis_before_loop', 'genesis_do_date_archive_title' );
-remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
-remove_action( 'genesis_before_loop', 'genesis_do_author_title_description', 15 );
-remove_action( 'genesis_before_loop', 'genesis_do_blog_template_heading' );
+remove_action('genesis_before_loop', 'genesis_do_cpt_archive_title_description');
+remove_action('genesis_before_loop', 'genesis_do_posts_page_heading');
+remove_action('genesis_before_loop', 'genesis_do_date_archive_title');
+remove_action('genesis_before_loop', 'genesis_do_taxonomy_title_description', 15);
+remove_action('genesis_before_loop', 'genesis_do_author_title_description', 15);
+remove_action('genesis_before_loop', 'genesis_do_blog_template_heading');
 
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_cpt_archive_title_description' );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_posts_page_heading', 15 );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_date_archive_title' );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_taxonomy_title_description', 17 );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_author_title_description', 17 );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_blog_template_heading', 15 );
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_cpt_archive_title_description');
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_posts_page_heading', 15);
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_date_archive_title');
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_taxonomy_title_description', 17);
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_author_title_description', 17);
+add_action('genesis_before_content_sidebar_wrap', 'genesis_do_blog_template_heading', 15);
 
-remove_filter( 'genesis_attr_entry-title-link', 'genesis_attributes_entry_title_link' );
-add_filter( 'genesis_attr_entry-title-link', 'empower_pro_blocks_attributes_entry_title_link' );
+remove_filter('genesis_attr_entry-title-link', 'genesis_attributes_entry_title_link');
+add_filter('genesis_attr_entry-title-link', 'empower_pro_blocks_attributes_entry_title_link');
 /**
  * Add attributes for entry title link.
  *
@@ -338,16 +340,16 @@ add_filter( 'genesis_attr_entry-title-link', 'empower_pro_blocks_attributes_entr
  * @param array $attributes Existing attributes for entry title element.
  * @return array Amended attributes for entry title element.
  */
-function empower_pro_blocks_attributes_entry_title_link( $attributes ) {
+function empower_pro_blocks_attributes_entry_title_link($attributes)
+{
 
 	$post_format = get_post_format();
 
 	$target = '_self';
-	if ( $post_format == "link" ) {
+	if ($post_format == "link") {
 		$permalink = empower_pro_blocks_get_link_url();
 		$target = "_blank";
-	}
-	else {
+	} else {
 		$permalink = get_permalink();
 	}
 
@@ -356,5 +358,4 @@ function empower_pro_blocks_attributes_entry_title_link( $attributes ) {
 	$attributes['target'] = $target;
 
 	return $attributes;
-
 }
--- lib/images.php
+++ lib/images.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Empower Pro Blocks.
  *
@@ -15,36 +16,37 @@
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_featured_image() {
+function empower_pro_blocks_featured_image()
+{
 
 	$featured_image = '';
 	global $empower_pro_blocks_appearance;
 
-	if ( is_404() && ! $empower_pro_blocks_appearance['page-image'] ) {
+	if (is_404() && !$empower_pro_blocks_appearance['page-image']) {
 		return;
 	}
 
-	if ( is_singular( 'post' ) && ! $empower_pro_blocks_appearance['post-image'] ) {
+	if (is_singular('post') && !$empower_pro_blocks_appearance['post-image']) {
 		return;
 	}
 
-	if ( is_singular( 'page' ) && ! $empower_pro_blocks_appearance['page-image'] ) {
+	if (is_singular('page') && !$empower_pro_blocks_appearance['page-image']) {
 		return;
 	}
 
-	if ( is_singular( 'portfolio' ) && ! $empower_pro_blocks_appearance['portfolio-image'] ) {
+	if (is_singular('portfolio') && !$empower_pro_blocks_appearance['portfolio-image']) {
 		return;
 	}
 
-	if ( is_singular( 'event' ) && ! $empower_pro_blocks_appearance['event-image'] ) {
+	if (is_singular('event') && !$empower_pro_blocks_appearance['event-image']) {
 		return;
 	}
 
 	$image_id = empower_pro_blocks_get_post_thumbnail_id();
 	$class    = '';
 
-	if ( $image_id ) {
-		$image_alt      = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+	if ($image_id) {
+		$image_alt      = get_post_meta($image_id, '_wp_attachment_image_alt', true);
 		$featured_image = genesis_get_image(
 			array(
 				'format'  => 'html',
@@ -57,24 +59,23 @@ function empower_pro_blocks_featured_image() {
 			)
 		);
 
-		$caption = wp_get_attachment_caption( $image_id );
+		$caption = wp_get_attachment_caption($image_id);
 	}
 
-	if ( $featured_image && ( is_singular( array( 'post', 'page', 'portfolio', 'event' ) ) || is_404() ) ) {
-		if ( ! empty( $caption ) ) {
+	if ($featured_image && (is_singular(array('post', 'page', 'portfolio', 'event')) || is_404())) {
+		if (!empty($caption)) {
 			$caption = '<figcaption>' . $caption . '</figcaption>';
 		}
 
 		$html = '<div class="full-width-image' . $class . '">' . $featured_image . $caption . '</div>';
-		echo wp_kses_post( $html );
+		echo wp_kses_post($html);
 	}
 
-	$button_html = genesis_get_custom_field( 'empower_pro_blocks_featured_button' );
+	$button_html = genesis_get_custom_field('empower_pro_blocks_featured_button');
 
-	if ( $button_html ) {
-		echo wp_kses_post( $button_html );
+	if ($button_html) {
+		echo wp_kses_post($button_html);
 	}
-
 }
 
 /**
@@ -82,36 +83,37 @@ function empower_pro_blocks_featured_image() {
  *
  * @since 1.0.0
  */
-function empower_pro_blocks_featured_image_2() {
+function empower_pro_blocks_featured_image_2()
+{
 
 	$featured_image = '';
 	global $empower_pro_blocks_appearance;
 
-	if ( is_404() && ! $empower_pro_blocks_appearance['page-image'] ) {
+	if (is_404() && !$empower_pro_blocks_appearance['page-image']) {
 		return;
 	}
 
-	if ( is_singular( 'post' ) && ! $empower_pro_blocks_appearance['post-image'] ) {
+	if (is_singular('post') && !$empower_pro_blocks_appearance['post-image']) {
 		return;
 	}
 
-	if ( is_singular( 'page' ) && ! $empower_pro_blocks_appearance['page-image'] ) {
+	if (is_singular('page') && !$empower_pro_blocks_appearance['page-image']) {
 		return;
 	}
 
-	if ( is_singular( 'portfolio' ) && ! $empower_pro_blocks_appearance['portfolio-image'] ) {
+	if (is_singular('portfolio') && !$empower_pro_blocks_appearance['portfolio-image']) {
 		return;
 	}
 
-	if ( is_singular( 'event' ) && ! $empower_pro_blocks_appearance['event-image'] ) {
+	if (is_singular('event') && !$empower_pro_blocks_appearance['event-image']) {
 		return;
 	}
 
 	$image_id = empower_pro_blocks_get_post_thumbnail_id();
 	$class    = '';
 
-	if ( $image_id ) {
-		$image_alt      = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+	if ($image_id) {
+		$image_alt      = get_post_meta($image_id, '_wp_attachment_image_alt', true);
 		$featured_image = genesis_get_image(
 			array(
 				'format'  => 'html',
@@ -127,20 +129,19 @@ function empower_pro_blocks_featured_image_2() {
 		// $caption = wp_get_attachment_caption( $image_id );
 	}
 
-	if ( $featured_image && ( is_singular( array( 'post', 'page', 'portfolio', 'event' ) ) || is_404() ) ) {
+	if ($featured_image && (is_singular(array('post', 'page', 'portfolio', 'event')) || is_404())) {
 		// if ( ! empty( $caption ) ) {
-			// $caption = '<figcaption>' . $caption . '</figcaption>';
+		// $caption = '<figcaption>' . $caption . '</figcaption>';
 		// }
 
 		$html = '<div class="featured-image-2' . $class . '">' . $featured_image . '</div>';
-		echo wp_kses_post( $html );
+		echo wp_kses_post($html);
 	}
-
 }
 
 // Move post image above post title.
-remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-add_action( 'genesis_entry_header', 'empower_pro_blocks_do_post_image', 1 );
+remove_action('genesis_entry_content', 'genesis_do_post_image', 8);
+add_action('genesis_entry_header', 'empower_pro_blocks_do_post_image', 1);
 /**
  * Echo the post image on archive pages.
  *
@@ -149,25 +150,25 @@ add_action( 'genesis_entry_header', 'empower_pro_blocks_do_post_image', 1 );
  *
  * @since 1.1.0
  */
-function empower_pro_blocks_do_post_image() {
+function empower_pro_blocks_do_post_image()
+{
 	$post_format = get_post_format();
 
-	if ( ! is_singular() && genesis_get_option( 'content_archive_thumbnail' ) ) {
-		if ( in_array( $post_format, array( 'audio', 'video' ) ) ) {
-			$media = $content = apply_filters( 'the_content', empower_pro_blocks_get_first_paragraph( get_the_content() ) );
-		}
-		else {
+	if (!is_singular() && genesis_get_option('content_archive_thumbnail')) {
+		if (in_array($post_format, array('audio', 'video'))) {
+			$media = $content = apply_filters('the_content', empower_pro_blocks_get_first_paragraph(get_the_content()));
+		} else {
 			$img = genesis_get_image(
 				[
 					'format'  => 'html',
-					'size'    => genesis_get_option( 'image_size' ),
+					'size'    => genesis_get_option('image_size'),
 					'context' => 'archive',
-					'attr'    => genesis_parse_attr( 'entry-image', [] ),
+					'attr'    => genesis_parse_attr('entry-image', []),
 				]
 			);
 		}
 
-		if ( ! empty( $img ) ) {
+		if (!empty($img)) {
 			genesis_markup(
 				[
 					'open'    => '<a %s>',
@@ -178,7 +179,7 @@ function empower_pro_blocks_do_post_image() {
 			);
 		}
 
-		if ( ! empty( $media ) ) {
+		if (!empty($media)) {
 			genesis_markup(
 				[
 					'open'    => '<div %s>',
@@ -191,8 +192,8 @@ function empower_pro_blocks_do_post_image() {
 	}
 }
 
-remove_filter( 'genesis_attr_entry-image-link', 'genesis_attributes_entry_image_link' );
-add_filter( 'genesis_attr_entry-image-link', 'empower_pro_blocks_attributes_entry_image_link' );
+remove_filter('genesis_attr_entry-image-link', 'genesis_attributes_entry_image_link');
+add_filter('genesis_attr_entry-image-link', 'empower_pro_blocks_attributes_entry_image_link');
 /**
  * Add attributes for entry title link.
  *
@@ -201,16 +202,16 @@ add_filter( 'genesis_attr_entry-image-link', 'empower_pro_blocks_attributes_entr
  * @param array $attributes Existing attributes for entry title element.
  * @return array Amended attributes for entry title element.
  */
-function empower_pro_blocks_attributes_entry_image_link( $attributes ) {
+function empower_pro_blocks_attributes_entry_image_link($attributes)
+{
 
 	$post_format = get_post_format();
 
 	$target = '_self';
-	if ( $post_format == "link" ) {
+	if ($post_format == "link") {
 		$permalink = empower_pro_blocks_get_link_url();
 		$target = "_blank";
-	}
-	else {
+	} else {
 		$permalink = get_permalink();
 	}
 
@@ -221,11 +222,10 @@ function empower_pro_blocks_attributes_entry_image_link( $attributes ) {
 	$attributes['target']      = $target;
 
 	return $attributes;
-
 }
 
-add_filter( 'genesis_get_image', 'empower_pro_blocks_wrap_featured_images', 10, 2 );
-add_filter( 'genesis_markup_entry-image-link_content', 'empower_pro_blocks_wrap_featured_images', 10, 2 );
+add_filter('genesis_get_image', 'empower_pro_blocks_wrap_featured_images', 10, 2);
+add_filter('genesis_markup_entry-image-link_content', 'empower_pro_blocks_wrap_featured_images', 10, 2);
 /**
  * Adds a wrapper to featured images output by Genesis Featured Post,
  * Genesis Featured Page, and Genesis Portfolio widgets, as well as
@@ -239,19 +239,19 @@ add_filter( 'genesis_markup_entry-image-link_content', 'empower_pro_blocks_wrap_
  * @param array  $args The arguments passed to genesis_get_image.
  * @return string The new HTML output including the wrapper div.
  */
-function empower_pro_blocks_wrap_featured_images( $output, $args ) {
+function empower_pro_blocks_wrap_featured_images($output, $args)
+{
 
-	if ( strpos( $output, '<img' ) === false ) {
+	if (strpos($output, '<img') === false) {
 		return $output;
 	}
 
 	$is_archive_thumbnail   = 'entry-image-link' === $args['context'];
-	$is_post_or_page_widget = in_array( $args['context'], array( 'featured-post-widget', 'featured-page-widget' ), true );
+	$is_post_or_page_widget = in_array($args['context'], array('featured-post-widget', 'featured-page-widget'), true);
 
-	if ( $is_archive_thumbnail || $is_post_or_page_widget ) {
-		$output = sprintf( '<div class="empower-pro-blocks-featured-image">%s</div>', $output );
+	if ($is_archive_thumbnail || $is_post_or_page_widget) {
+		$output = sprintf('<div class="empower-pro-blocks-featured-image">%s</div>', $output);
 	}
 
 	return $output;
-
 }
